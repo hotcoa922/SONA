c73263a5397412b922fc7bf18d41c025223ec79d
hotcoa <tahen922@naver.com>
2022-05-02 23:46:29 +0900

    작업환경 재설정
    테스팅 환경 구축완료
    
    <mainactivity-기타 연결 fragment간 툴바연결 테스트> - 구현완료

--- app/src/main/java/com/hotcoa/sona/main/MainActivity.java
+++ app/src/main/java/com/hotcoa/sona/main/MainActivity.java
@@ -1,11 +1,15 @@
 package com.hotcoa.sona.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 import com.google.android.material.navigation.NavigationView;
@@ -21,35 +25,67 @@ public class MainActivity extends AppCompatActivity {
     UserGuideFragment guide;
     CalendarFragment calendar;
     NavigationView navi;
+
+    DrawerLayout drawerLayout;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //툴바 생성
         toolbar = (Toolbar) findViewById(R.id.toolbar_x);
         setSupportActionBar(toolbar);
 
+        //사이드 메뉴 오픈하기 위한 아이콘 add
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_48);  //아이콘 추가
+        getSupportActionBar().setDisplayShowTitleEnabled(false);// ActionBar 앱이름 감추기
 
         navi = (NavigationView) findViewById(R.id.navi_x);
         appset = new AppSettingFragment();
         guide = new UserGuideFragment();
         calendar = new CalendarFragment();
 
+        drawerLayout = findViewById(R.id.drawlayout_x);
 
 
+        //네비창 열면 연 다음 눌렀을 때 처리하는 것
         navi.setNavigationItemSelectedListener(menuItem->{
+
             if(menuItem.getItemId() == R.id.appsetting_navi){
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_x, appset).commit();
             }
-
+            if(menuItem.getItemId() == R.id.calaendar_navi){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_x,calendar ).commit();
+            }
             if(menuItem.getItemId() == R.id.guide_navi){
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_x,guide ).commit();
             }
-
+            drawerLayout.closeDrawer(GravityCompat.START);  //방향을 지정해 주는 것
 
             return true;
         });
     }
+
+    //ctrl+o 에서 오버라이드할꺼 검색 ㄱㄴ
+    //누르면 사이드 메뉴 열리는 것
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        //drawerLayout = findViewById(R.id.drawlayout_x);
+        if(item.getItemId()==android.R.id.home){
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
