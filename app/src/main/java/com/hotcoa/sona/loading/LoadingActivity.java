248b1739213123b7b5c72c608d1a97f85413e0e4
bbangmaru <hobin1217@soongsil.ac.kr>
2022-05-04 15:10:58 +0900

    loading 구현

--- app/src/main/java/com/hotcoa/sona/loading/LoadingActivity.java
+++ app/src/main/java/com/hotcoa/sona/loading/LoadingActivity.java
@@ -2,9 +2,12 @@ package com.hotcoa.sona.loading;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.hotcoa.sona.R;
+import com.hotcoa.sona.main.MainActivity;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -12,5 +15,18 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
