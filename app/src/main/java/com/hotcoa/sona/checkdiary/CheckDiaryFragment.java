eb8d9d8fc733505133eb2b35aa8489a7b11d6e6f
박근형 <w8385@naver.com>
2022-05-28 00:56:07 +0900

    CheckDiary 복호화 구현 완료

--- app/src/main/java/com/hotcoa/sona/checkdiary/CheckDiaryFragment.java
+++ app/src/main/java/com/hotcoa/sona/checkdiary/CheckDiaryFragment.java
@@ -2,10 +2,14 @@ package com.hotcoa.sona.checkdiary;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,8 +18,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hotcoa.sona.R;
+import com.hotcoa.sona.leacrypto.LEA_Crypto;
 import com.hotcoa.sona.main.MainActivity;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -45,7 +54,26 @@ public class CheckDiaryFragment extends Fragment {
         Button sharebt = rootView.findViewById(R.id.button_share);
 
         datetv.setText(getTime());
-        SharedPreferences saveDatePrefs = getActivity().getSharedPreferences("saveDate", Context.MODE_PRIVATE);
+        String diary_path = "/storage/emulated/0/SONA/text/" + getTime() + ".txt";
+        String saveData = readFile(diary_path);
+        Log.d("showtv 경로",diary_path);
+        Log.d("showtv 내용", readFile(diary_path));
+
+        SharedPreferences idPrefs = getActivity().getSharedPreferences("android_id", Context.MODE_PRIVATE);
+        String android_id = idPrefs.getString("android_id","");
+        try {
+            byte[] pbkdf_id = LEA_Crypto.PBKDF(android_id);
+            Log.d("checkDiary", "pbkdf_id = " + pbkdf_id.toString());
+
+            String decrypted = LEA_Crypto.decode(saveData, pbkdf_id);
+            Log.d("checkDiary", decrypted);
+
+            showtv.setText(decrypted);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d("lea error", e.toString());
+        }
+
 
         onEditClick(editbt);
         onDeleteClick(deletebt);
@@ -77,5 +105,55 @@ public class CheckDiaryFragment extends Fragment {
         SharedPreferences prefs = getActivity().getSharedPreferences("curDate", Context.MODE_PRIVATE);
         String temp = prefs.getString("curDate", dateFormat.format(date));
         return temp;
+    }
+
+    //TODO [MediaStore 파일 불러오기 실시]
+    private String readFile(String path) {
+        Log.d("---","---");
+        Log.d("//===========//","================================================");
+        Log.d("","\n"+"[A_ScopeTxt > readFile() 메소드 : MediaStore 파일 불러오기 실시]");
+        Log.d("","\n"+"[절대 파일 경로 : "+String.valueOf(path)+"]");
+        Log.d("//===========//","================================================");
+        Log.d("---","---");
+        Uri externalUri = MediaStore.Files.getContentUri("external");
+        String[] projection = new String[]{
+                MediaStore.Images.Media._ID,
+                MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.MIME_TYPE
+        };
+
+        Cursor cursor = getActivity().getContentResolver().query(externalUri, projection, null, null, null);
+
+        if (cursor == null || !cursor.moveToFirst()) {
+            Log.d("---","---");
+            Log.e("//===========//","================================================");
+            Log.d("","\n"+"[A_ScopeTxt > readFile() 메소드 : MediaStore 파일 불러오기 실패]");
+            Log.d("","\n"+"[원인 : "+String.valueOf("Cursor 객체 null")+"]");
+            Log.e("//===========//","================================================");
+            Log.d("---","---");
+            return "";
+        }
+
+        //TODO [텍스트 파일 불러오기 실시]
+        String absoluteUrl = path;
+        String line = ""; //TODO [한줄씩 읽기]
+        try {
+            BufferedReader buf = new BufferedReader(new FileReader(absoluteUrl));
+            while((line=buf.readLine())!=null){
+                Log.d("---","---");
+                Log.w("//===========//","================================================");
+                Log.d("showtv_내용","\n"+"[A_ScopeTxt > readFile() 메소드 : MediaStore 파일 불러오기 성공]");
+                Log.d("showtv_경로","\n"+"[절대 파일 경로 : "+String.valueOf(absoluteUrl)+"]");
+                Log.d("showtv_내용","\n"+"[절대 파일 내용 : "+String.valueOf(line)+"]");
+                Log.w("//===========//","================================================");
+                Log.d("---","---");
+                return String.valueOf(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
